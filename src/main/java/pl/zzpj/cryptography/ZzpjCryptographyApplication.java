6366c2e6d16bd746b3b27bd1ff4a9409a107df0b
Michał Krzywański <189705@edu.p.lodz.pl>
2016-05-26 23:55:15 +0200

    Dodanie pierwszego kontrolera i strony - trzeba pomyslec nad innym ViewResolverem

--- src/main/java/pl/zzpj/cryptography/ZzpjCryptographyApplication.java
+++ src/main/java/pl/zzpj/cryptography/ZzpjCryptographyApplication.java
@@ -3,10 +3,12 @@ package pl.zzpj.cryptography;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ZzpjCryptographyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZzpjCryptographyApplication.class, args);
+		
 	}
 }
