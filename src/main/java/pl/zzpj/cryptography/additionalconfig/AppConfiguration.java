87fb29edc03b0c366af73251b21db2d9b2bff167
Michał Krzywański <189705@edu.p.lodz.pl>
2016-08-24 18:14:42 +0200

    Poprawa jednego z testów, proba przesylania plikow

---
+++ src/main/java/pl/zzpj/cryptography/additionalconfig/AppConfiguration.java
@@ -0,0 +1,16 @@
+package pl.zzpj.cryptography.additionalconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+@Configuration
+public class AppConfiguration {
+	@Bean
+	public CommonsMultipartResolver multipartResolver() {
+	    CommonsMultipartResolver resolver=new CommonsMultipartResolver();
+	    resolver.setDefaultEncoding("utf-8");
+	    resolver.setMaxUploadSize(5000000);
+	    return resolver;
+	}
+}
--- src/main/java/pl/zzpj/cryptography/web/restapi/DESController.java
+++ src/main/java/pl/zzpj/cryptography/web/restapi/DESController.java
@@ -1,8 +1,10 @@
 package pl.zzpj.cryptography.web.restapi;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
 
 import pl.zzpj.cryptography.des.exceptions.InvalidKeyException;
 import pl.zzpj.cryptography.interfaces.IDes;
@@ -25,8 +28,9 @@ public class DESController {
 		this.des = des;
 	}
 	
-	@RequestMapping(path = "/encrypt/file", method = RequestMethod.POST)
-	public @ResponseBody HttpEntity encryptFile(@RequestParam("file") MultipartFile file, @RequestParam("key") String key) {
+	@RequestMapping(path = "/encrypt/file", method = RequestMethod.POST, consumes={"multipart/form-data"})
+	public @ResponseBody HttpEntity encryptFile(@RequestParam(value="file") MultipartFile file, @RequestParam("key") String key) {
+		System.out.println(file.isEmpty());
 		return new ResponseEntity(HttpStatus.OK);
 	}
 	
