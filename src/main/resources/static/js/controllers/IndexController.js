eae987bff7b41736aee7c96eb55be7347492d1e5
ignas456 <michal.ignaczewski@outlook.com>
2016-08-28 17:24:44 +0200

    Frontend spięty z backendem

--- src/main/resources/static/js/controllers/IndexController.js
+++ src/main/resources/static/js/controllers/IndexController.js
@@ -8,9 +8,38 @@
     IndexController.$inject = ['$scope','Upload', '$http'];
     
     function IndexController($scope, Upload, $http) { 
-    	$scope.testText = "AngularDziala";
-    	
-    	$scope.uploadFile=function(files){
+        'use strict';
+
+    	var self = this;
+        var keyPattern = /^[0-9a-fA-F]{8}$/;
+
+    	self.inputType="text";
+
+    	self.key = '';
+    	self.isKeyValid = false;
+
+    	self.textInput = '';
+    	self.textOutput = '';
+
+        self.validateKey = function(){
+            keyPattern.test(self.key);
+            console.log(keyPattern.test(self.key));
+        }
+    	self.encrypt = function(){
+            $http.post('api/des/encrypt/text?text=' + self.textInput + '&key=' + self.key)
+            .then(function(response){
+                self.textOutput = response.data;
+            });
+    	}
+
+    	self.decrypt = function(){
+    	    $http.post('api/des/decrypt/text?text=' + self.textInput + '&key=' + self.key)
+                .then(function(response){
+                    self.textOutput = response.data;
+                });
+    	}
+
+    	self.encryptFile=function(files){
     	    var formData=new FormData();
     	    console.log(files)
     	    formData.append("key","asdaasda")
@@ -31,6 +60,17 @@
     	            alert("Error ... " + status);
     	        });*/
     	}
+
+    	self.decryptFile = function(files){
+            var formData=new FormData();
+            console.log(files)
+            formData.append("key","asdaasda")
+            formData.append("file", files[0]);
+
+            var request = new XMLHttpRequest();
+            request.open('POST', 'api/des/decrypt/file');
+            request.send(formData);
+        }
     }
 })();
 
